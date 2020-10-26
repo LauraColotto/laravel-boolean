091d177aa692c207c4f0c2652d8a28e19b6962a5
Lau <arinel88@gmail.com>
2020-10-26 23:57:06 +0100

    Fatto foreach per stampare a video la tabella del database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,11 @@
-@dd($students)
+<table>
+    @foreach ($students as $student)
+        <tr>
+            <td>{{$student->name}}</td>
+            <td>{{$student->lastname}}</td>
+            <td>{{$student->email}}</td>
+            <td>{{$student->github}}</td>
+        </tr>
+        
+    @endforeach
+</table>
